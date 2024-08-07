0c96a0c927a87c151f81b73b2218ffb32e2e090f
juanitazoeyp <112683950+juanitazoeyp@users.noreply.github.com>
2024-08-07 12:24:57 +0200

    Update beautifulsoup.py
    added entry point

--- beautifulsoup.py
+++ beautifulsoup.py
@@ -1,19 +1,24 @@
+
 import requests
 from bs4 import BeautifulSoup
 
-URL = 'https://www.bbc.com/weather/964420'
-page = requests.get(URL)
-soup = BeautifulSoup(page.text, 'html5lib')
+def main():
+  URL = 'https://www.bbc.com/weather/964420'
+  page = requests.get(URL)
+  soup = BeautifulSoup(page.text, 'html5lib')
 
-print("Below is some information scraped from a website that has some information regarding todays weather:")
+  print("Below is some information scraped from a website that has some information regarding todays weather:")
 
-s = soup.find_all('span', class_ = 'wr-value--temperature--c')
-print("Today's high temp: ")
-s1 = s[0].text
-print(s1)
-print("Today's low temp: ")
-s2 = s[1].text
-print(s2)
-if name == '__main__':
-  print()
+  s = soup.find_all('span', class_ = 'wr-value--temperature--c')
+  print("Today's high temp: ")
+  s1 = s[0].text
+  print(s1)
+  print("Today's low temp: ")
+  s2 = s[1].text
+  print(s2)
+
+if __name__ == '__main__':
+  main()
+  
+ 
   
